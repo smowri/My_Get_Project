b530e6e7a15e3bb2d5c5498b8595cd23dc420322
Mowri <mselvaku@wipro.com>
2018-01-30 15:29:05 +0530

    My first commit

---
+++ My_Java_Hello.java
@@ -0,0 +1,24 @@
+
+public class My_Java_Hello {
+
+	public static void main(String[] args) {
+
+System.out.println("Hello world");
+		
+		if (true) 
+		{
+		System.out.println("I am If clause");
+		}
+		if (false)
+		{
+		System.out.println("false message");
+		}
+		System.out.println("I am done"); 
+		if ( false)
+		{
+			System.out.println("Dead code");
+		}
+		System.out.println("Dead code");
+	}
+
+}
